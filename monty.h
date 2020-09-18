c7a68ee9031abbabefffee58f05768c60b825343
Camilo <camilo.pulgarin11@gmail.com>
2020-09-17 23:43:36 -0500

    Hot fix point 1

--- monty.h
+++ monty.h
@@ -66,5 +66,6 @@ void erase_list(void);
 
 void push(stack_t **head, unsigned int command_line);
 void pall(stack_t **stack, unsigned int command_line);
+void pint(stack_t **head, unsigned int command_line);
 
 #endif
